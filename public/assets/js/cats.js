c8b0312d7c78265bfcdbe6ea9237a01889204e32
Rory James <rjlanam@gmail.com>
2017-08-29 20:10:53 -0500

    random cat

--- public/assets/js/cats.js
+++ public/assets/js/cats.js
@@ -1,10 +1,17 @@
 //pull all json data
-//select a random cat to display on html
-var randomNum = Math.floor(Math.random() * (20 - 1) + 1);
 
-$.get('/api/cat/', function(req, res) {
-
-}).then(function(data, res) {
-    res.JSON(data);
+$.get('/api/cat/', function(data) {
     console.log(data);
+
+    //select a random cat to display on html
+    var randomNum = Math.floor(Math.random() * (30 - 1) + 1);
+
+    $('#well-section').append('<img class="cat" src=' + data[randomNum].picture + '>');
+
+    $('#like').on('click', function() {
+        app.post('/api/like', function(req, res) {
+            res++;
+        })
+    })
+
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,11 @@
-var People = require("../models/catModel.js");
+var Cats = require("../models/catModel.js");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
 
     app.get("/api/cat/", function(req, res) {
-        People.findAll({}).then(function(results) {
+        Cats.findAll({}).then(function(results) {
             res.json(results);
         });
     });
